1bc3570b4caeb0d0e83a73d0634305f2b39a456d
SHENA <35950709+shena1125@users.noreply.github.com>
2026-07-27 18:32:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,8 +18,12 @@ async function loadSongs() {
         <div class="song-title">${song.song_title}</div>
         <div class="artist">${song.artist}</div>
         <div>初披露：${song.first ? "✔" : "－"}</div>
-        <a class="live-link" href="${song.live_url}" target="_blank">配信を見る</a>
-      `;
+       card.innerHTML = `
+  <div class="song-title">${song.song_title}</div>
+  <div class="artist">${song.artist}</div>
+  <div>初披露：${song.first ? "✔" : "－"}</div>
+  <a class="live-link" href="${song.live_url}?t=${song.start_seconds}" target="_blank">配信を見る</a>
+`;
 
       list.appendChild(card);
     });
